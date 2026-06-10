aff0d5bfd4ef3c7082ad6f3a59d697e0582b66a9
Rahman-Zebua <rachzebua@gmail.com>
2026-06-10 15:11:50 +0700

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,53 @@
+```javascript
 let total = 0;
+
+function buatProduk() {
+
+  const nama =
+    document.getElementById("namaProduk").value;
+
+  const harga =
+    parseInt(document.getElementById("hargaProduk").value);
+
+  if (!nama || !harga) {
+
+    alert("Isi nama dan harga produk!");
+
+    return;
+  }
+
+  const divProduk =
+    document.querySelector(".produk");
+
+  const tombol =
+    document.createElement("button");
+
+  tombol.innerText =
+    `${nama} - Rp${harga}`;
+
+  tombol.onclick = function () {
+
+    tambahProduk(nama, harga);
+  };
+
+  divProduk.appendChild(tombol);
+
+  document.getElementById("namaProduk").value = "";
+
+  document.getElementById("hargaProduk").value = "";
+}
 
 function tambahProduk(nama, harga) {
 
-  const keranjang = document.getElementById("keranjang");
+  const keranjang =
+    document.getElementById("keranjang");
 
-  const item = document.createElement("li");
+  const item =
+    document.createElement("li");
 
   item.innerHTML = `
     ${nama} - Rp${harga}
+
     <button onclick="hapusItem(this, ${harga})">
       Hapus
     </button>
@@ -45,12 +85,21 @@ function resetKasir() {
 
   document.getElementById("kembalian").innerText =
     "Kembalian: Rp0";
+
+  document.getElementById("bayar").value = "";
 }
 
 function hitungKembalian() {
 
   const bayar =
     parseInt(document.getElementById("bayar").value);
+
+  if (!bayar) {
+
+    alert("Masukkan uang bayar!");
+
+    return;
+  }
 
   const kembali = bayar - total;
 
@@ -67,3 +116,4 @@ function toggleDarkMode() {
 
   document.body.classList.toggle("dark");
 }
+```
